0a0095b961c4aca6b03db84777015ac8fe060aeb
Angela Mitchell <angela.mitchell@vmlyr.com>
2022-03-22 15:08:15 -0500

    added javascript functionality for search bar

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 const userCardsContainer = document.querySelector('[data-user-cards-container]');
 const userCardTemplate = document.querySelector("[data-user-template]");
+const searchInput = document.querySelector("[data-search]");
+
+let users = [];
+
+searchInput.addEventListener('input', (event) => {
+    const value = event.target.value.toLowerCase();
+    console.log(users);
+    users.forEach((user) => {
+        const isVisible = user.name.toLowerCase().includes(value) || user.email.toLowerCase().includes(value);
+        user.element.classList.toggle('hide', !isVisible);
+    })
+});
 
 fetch('https://jsonplaceholder.typicode.com/users')
     .then((response) => response.json())
     .then((data) => {
-        data.forEach((user) => {
+        users = data.map((user) => {
             const card = userCardTemplate.content.cloneNode(true).children[0];
             const name = card.querySelector('[data-name]');
             const email = card.querySelector('[data-email]');
@@ -13,5 +25,7 @@ fetch('https://jsonplaceholder.typicode.com/users')
             email.innerHTML = user.email;
 
             userCardsContainer.append(card);
+
+            return { name: user.name, email: user.email, element: card };
         })
     });
